c1ed9e51fed4f2b44c46824925157507e3ac4cc6
Hanner <Hannerb48@gmail.com>
2024-02-24 22:06:23 -0500

    Creando el import

--- resources/views/export.blade.php
+++ resources/views/export.blade.php
@@ -1,2 +1,8 @@
-<h1>Exportar contenidos a Excel</h1>
-<a href="{{ route('export') }}">Exportar</a>
+<link rel="stylesheet" href="{!! asset('css/app.css') !!}">
+<div class="container">
+    <div>
+
+        <h1>Exportar contenidos a Excel</h1>
+        <a href="{{ route('export') }}">Exportar</a>
+    </div>
+</div>
